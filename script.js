6f115864fd6084ee4dd73ed8f612322833b8e1de
Aaxtrox <pawelw@gmx.com>
2023-05-23 11:57:42 +0100

    effects added for add button

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+addButton = document.getElementById('addButton');
+discard = document.getElementById('discard');
+addButton.addEventListener('click', () => {
+    // change pop up display to block
+    document.getElementById('popup').style.display = 'block';
+    //disable hoover effect on addButton
+    addButton.style.pointerEvents = 'none';
+    //shade out effect on body
+    document.getElementsByTagName('body')[0].style.backgroundColor = 'rgba(0,0,0,0.2)';
+});
+
+discard.addEventListener('click', () => {
+    // change pop up display to none
+    document.getElementById('popup').style.display = 'none';
+    //enable hoover effect on addButton
+    addButton.style.pointerEvents = 'auto';
+    //body grade out effect
+    document.getElementsByTagName('body')[0].style.backgroundColor = 'white';
+});
+
 let myLibrary = [];
 
 function Book() {
