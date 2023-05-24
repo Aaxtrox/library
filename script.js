12ab4ab108bef3eeaeeb8ebb688220a5cf273ed2
Aaxtrox <pawelw@gmx.com>
2023-05-24 12:13:21 +0100

    code clean up

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
+// grab elements
 addButton = document.getElementById('addButton');
 discard = document.getElementById('discard');
 
+// addButton event listeners to display pop up
 addButton.addEventListener('click', () => {
     // change pop up display to block
     document.getElementById('popup').style.display = 'block';
     //disable hoover effect on addButton
     addButton.style.pointerEvents = 'none';
-    //shade out effect on body
-    document.getElementsByTagName('body')[0].style.backgroundColor = 'rgba(0,0,0,0.2)';
 });
 
+// discard event listeners to hide pop up
 discard.addEventListener('click', () => {
     // change pop up display to none
     document.getElementById('popup').style.display = 'none';
